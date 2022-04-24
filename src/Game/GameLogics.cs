c00294043bcd468df783c745d1b628eec3cbb866
Ardavan <ardavannafezi@gmail.com>
2022-04-24 11:37:04 +0430

    logic added

---
+++ src/Game/GameLogics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class GameLogics
+    {
+
+        public void GenerateNumber()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                CheckNumber(i);
+            }
+        }
+
+        public void CheckNumber(int number)
+        {
+            if (number % 5 == 0 && number % 3 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if ( number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+        }
+    }
+}
---
+++ src/GameTest/GameTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GameTest
+{
+    public class Gametest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
